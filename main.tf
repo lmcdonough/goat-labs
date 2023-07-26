81027b9a0e966e05189ed0396a8de031c29242f3
Levi McDonough <levimcdonough@gmail.com>
2023-07-25 22:52:16 -0400

    attempting to fix vpc module by adding params at root main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,18 @@ provider "aws" {
 
 # import the vpc module
 module "vpc" {
-  source = "./modules/vpc"
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "5.1.0"
+  name               = var.vpc_name
+  cidr               = var.vpc_cidr
+  azs                = var.vpc_azs
+  private_subnets    = var.vpc_private_subnets
+  public_subnets     = var.vpc_public_subnets
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
+  tags = {
+    Terraform   = var.terraform_tag
+    Environment = var.environment_tag
+  }
+
 }
